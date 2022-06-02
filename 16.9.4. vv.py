986ae4bffbaac7b420ed3381a64bc2aa982fe015
BulychevMikhail <102511691+BulychevMikhail@users.noreply.github.com>
2022-06-02 22:13:51 +0400

    Add files via upload

---
+++ 16.9.4. vv.py
@@ -0,0 +1,23 @@
+"""Online wallet"""
+
+print('\n<<< Online wallet >>>')
+class Clients:
+    def __init__(self, name, surname, city, balance):
+        self.name = name
+        self.surname = surname
+        self.city = city
+        self.balance = balance
+    def __str__(self):
+        return f'{self.name} {self.surname}, г.{self.city}, Баланс: {self.balance} руб.'
+
+    def guests(self):
+        return f'{self.name} {self.surname}, г.{self.city}'
+
+client1 = Clients('Иван', 'Петров', 'Москва', 50)
+client2 = Clients('Радомир', 'Светлов', 'Аркаим', 120)
+client3 = Clients('Влад', 'Васнецов', 'Самара', 87)
+#print(f"client 1: {client1}")
+
+guests_list = [client1, client2, client3]
+for guest in guests_list:
+    print(guest.guests())
